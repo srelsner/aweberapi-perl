61cb1fa629de7757441ebf7c05834b02b8c7f757
stevee <stevee@aweber.com>
2016-11-01 17:14:34 -0400

    Add documentation showing token loading from file

--- demo.pl
+++ demo.pl
@@ -3,6 +3,23 @@ use JSON;
 
 use Data::Dumper;
 
+=head1 DEMO
+
+Simple demo of how to use the Net::aweberapi module
+
+=head2 Getting Started
+
+This script, as written, shows how to hard code your access tokens.
+In production, you probably don't want to do that. One alternative is
+to load the tokens from an init file. Net::OAuth::Simple provides convenience
+functions for that. In the context of this demo, it could look like this:
+
+    my $CONFIG = ".aweberapi_config";
+    my %tokens = Net::OAuth::Simple->load_tokens($CONFIG);
+    my $aw = Net::AWeberAPI->new(%tokens);
+
+=cut
+
 my $aw = Net::AWeberAPI->new( consumer_key => '',
                               consumer_secret => '',
                               access_token => '',
